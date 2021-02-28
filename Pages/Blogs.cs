35b7a9c8d758e2f3f9fc648258bde54e58ffaf58
Kael <kaelanreece@gmail.com>
2021-02-28 14:02:57 +1000

    added razor component for rendering the list of blogs. Updated blogs and index to use this.

--- Pages/Blogs.cs
+++ Pages/Blogs.cs
@@ -1,9 +1,8 @@
-using System.Linq;
 using System.Collections.Generic;
-using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
-using BlogWebsite.Helper;
 using BlogWebsite.Models;
+using BlogWebsite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -11,31 +10,17 @@ namespace BlogWebsite.Pages
 {
 	public class BlogListModel : PageModel
 	{
-		public BlogListModel()
+		private readonly IBlogService _blogService;
+		public BlogListModel(IBlogService blogService)
 		{
+			_blogService = blogService;
 			Blogs = new List<BlogPost>();
 		}
 
 		public IEnumerable<BlogPost> Blogs { get; set; }
 		public async Task<IActionResult> OnGetAsync()
 		{
-			var files = Directory.GetFiles("Content/Blogs", "*.md");
-
-			var blogList = new List<BlogPost>();
-			// loop through all of the markdown files in the blogs folder and add to
-			foreach (var file in files)
-			{
-				var markdownFile = await System.IO.File.ReadAllTextAsync(file);
-				var docBuilder = new MarkDownDocBuilder<BlogPost>(markdownFile)
-					.WithYamlData()
-					.Build();
-
-				if (docBuilder.YamlData is not null)
-				{
-					blogList.Add(docBuilder.YamlData);
-				}
-			}
-			Blogs = blogList.OrderByDescending(b => b.Published);
+			Blogs = (await _blogService.GetBlogPostsAsync()).OrderByDescending(b => b.Published);
 			return Page();
 		}
 	}
--- Pages/Index.cs
+++ Pages/Index.cs
@@ -1,4 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogWebsite.Helper;
+using BlogWebsite.Models;
+using BlogWebsite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace BlogWebsite.Pages
@@ -6,15 +14,20 @@ namespace BlogWebsite.Pages
 	public class IndexModel : PageModel
 	{
 		private readonly ILogger<IndexModel> _logger;
+		private readonly IBlogService _blogService;
 
-		public IndexModel(ILogger<IndexModel> logger)
+		public IndexModel(ILogger<IndexModel> logger, IBlogService blogService)
 		{
 			_logger = logger;
+			_blogService = blogService;
+			Blogs = new List<BlogPost>();
 		}
 
-		public void OnGet()
+		public IEnumerable<BlogPost> Blogs { get; set; }
+		public async Task<IActionResult> OnGetAsync()
 		{
-
+			Blogs = (await _blogService.GetBlogPostsAsync()).OrderByDescending(b => b.Published).Take(4);
+			return Page();
 		}
 	}
 }
